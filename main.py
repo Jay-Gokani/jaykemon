20f6d489f982082e4e5fcdacd094bcb7e44711a2
Jay-Gokani <112378055+Jay-Gokani@users.noreply.github.com>
2023-08-18 15:47:16 +0100

    building out choices off of main choice

--- main.py
+++ main.py
@@ -39,6 +39,9 @@ def turn():
 
     print('')
     sleep(1)
+    print('Haunter (HP 20/20) vs Kadabra (HP 18/18)')
+    print('')
+    sleep(1)
     print('1 Fight')
     sleep(1)
     print('2 Item')
@@ -46,6 +49,7 @@ def turn():
     print('3 Pokemon')
     sleep(1)
     print('4 Run')
+    sleep(1)
     print('')
     print('==================================================')
     sleep(1)
@@ -56,21 +60,25 @@ def turn():
         if main_choice == '1':
             sleep(1)
             print('You chose: Fight')
+            sleep(3)
             fight_choice()
             return False
         elif main_choice == '2':
             sleep(1)
             print('You chose: Item')
+            sleep(3)
             item_choice()
             return False
         elif main_choice == '3':
             sleep(1)
             print('You chose: Pokemon')
+            sleep(3)
             pokemon_choice()
             return False
         elif main_choice == '4':
             sleep(1)
             print('You chose: Run')
+            sleep(3)
             run_choice()
             return False
         else:
@@ -80,13 +88,55 @@ def turn():
             turn()
 
 def fight_choice():
-    print('Out of the loop')
+    print('Update me!')
 
 def item_choice():
-    print('Out of the loop')
+    game_title()
+    sleep(1)
+    print('')
+    print('Items')
+    print('')
+    sleep(1)
+    print('1 Potion')
+    sleep(1)
+    print('2 Randopotion')
+    sleep(1)
+    print('3 Pokeball')
+    sleep(1)
+    print('')
+    print('==================================================')
+    sleep(1)
+
+    while True:
+        item_selected = input('Type a number to select an option: ')
+        if item_selected == '1':
+            print(2)
+            # Todo: make a Haunter instance
+            # Haunter HP + 20
+            # If HP > max: max
+            # Print 'Haunter's HP is now x'
+        elif item_selected == '2':
+            print(2)
+            # Restore health by randint between 0 and (max health - current health)
+        elif item_selected == '3':
+            sleep(1)
+            print('You can\'t catch another trainer\'s Pokemon! Select another item...')
+            sleep(4)
+            item_choice()
+            return False
+        else:
+            sleep(1)
+            print('Please only select a choice between 1 and 3...')
+            sleep(4)
+            item_choice()
+            return False
+            
 
 def pokemon_choice():
-    print('Out of the loop')
+    sleep(1)
+    print('You currently have no other Pokemon in your party. Select another choice...')
+    sleep(4)
+    turn()
 
 def run_choice():
     sleep(1)
